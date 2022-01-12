a819aee3f8289b8bc174e73991d2eb7b7c710f2d
kearney <191615342@qq.com>
2022-01-12 11:22:45 +0800

    模板

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>欢迎访问我的博客首页！</h1>")
+    return render(request, 'blog/index.html', context={
+        'title': '首页',
+        'welcome': '欢迎访问我的博客首页'
+    })

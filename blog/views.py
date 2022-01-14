7118238982bb7563e36de00f23bfb4086f26411d
kearney <191615342@qq.com>
2022-01-14 14:08:05 +0800

    添加对markdown的支持

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+import markdown
 
 
 def index(request):
@@ -8,5 +9,12 @@ def index(request):
 
 
 def detail(request, pk):
+    """将 Markdown 格式的文本解析成 HTML 文本"""
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})

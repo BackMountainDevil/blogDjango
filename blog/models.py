f02ea7db3f013971dcd229a0ff7ee74a61b1ab63
kearney <191615342@qq.com>
2022-01-14 16:11:15 +0800

    在模型中指定排序方式

--- blog/models.py
+++ blog/models.py
@@ -84,6 +84,7 @@ class Post(models.Model):
     class Meta:
         verbose_name = '文章'
         verbose_name_plural = verbose_name  # 复数显示形式
+        ordering = ['-created_time']
 
     def __str__(self):
         return self.title

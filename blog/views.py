f931cb2a66f46ef54f2d6072b944cd7dda4b3db8
kearney <191615342@qq.com>
2022-01-14 14:14:33 +0800

    锚点美化，可显示中文

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 import markdown
+import re   # 正则化
+from django.utils.text import slugify   # 标题锚点美化，可显示中文
+from markdown.extensions.toc import TocExtension
 
 
 def index(request):
@@ -11,10 +14,13 @@ def index(request):
 def detail(request, pk):
     """将 Markdown 格式的文本解析成 HTML 文本"""
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
     return render(request, 'blog/detail.html', context={'post': post})
